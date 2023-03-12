c9669e9c6ff9e867cf4af7a2b8176f0b3636cedf
swapnilchavan13 <chavanswapnil822@gmail.com>
2023-03-12 16:42:59 +0530

    updated

--- index.js
+++ index.js
@@ -4,60 +4,65 @@ const mongoose = require("mongoose")
 const cors = require("cors")
 const Book = require('./models/books')
 
-const app=express();
-const PORT= process.env.PORT || 3000;
+const app = express();
+const PORT = process.env.PORT || 3000;
 
 mongoose.set('strictQuery', false);
 
-const connectDB =async ()=>{
-    try {
-        const conn = await mongoose.connect(process.env.MONGO_URL);
-        console.log((`MongoDb Connected : ${conn.connection.host}`))
-    } catch (error) {
-        process.exit(1)
-    }
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URL);
+    console.log((`MongoDb Connected : ${conn.connection.host}`))
+  } catch (error) {
+    process.exit(1)
+  }
 }
 
+// Enable CORS for all routes
 app.use(cors());
 
-app.get('/', (req, res)=>{
-res.send({Title:"HomePage"})
+app.get('/', (req, res) => {
+  res.send({Title: "HomePage"})
 })
 
 app.get('/add-note', async (req, res) => {
-    try {
-        await Book.insertMany([
-            
-        ])
-        
-    } catch (error) {
-        console.log("err", + error)
-    }
+  try {
+    await Book.insertMany([
+    
+    ])
+    
+  } catch (error) {
+    console.log("err", + error)
+  }
 })
 
-app.get('/books' ,async (req,res) => {
-    const book = await Book.find();
-
-if (book){
-    res.json(book)
-}else{
-    res.send("Something went Wrong")
-}
-})
-app.get('/books/:_id' ,async (req,res) => {
-    const bookId = req.params._id;
-    const book = await Book.findById(bookId);
-
-    if (book){
-        res.json(book)
-    }else{
-        res.status(404).send("Book not found")
+// Get single book by id
+app.get('/books/:_id', async (req, res) => {
+  try {
+    const book = await Book.findById(req.params._id);
+    
+    if (book) {
+      res.json(book)
+    } else {
+      res.send("Book not found")
     }
+  } catch (error) {
+    res.send(error.message)
+  }
+})
+
+app.get('/books', async (req, res) => {
+  const books = await Book.find();
+
+  if (books) {
+    res.json(books)
+  } else {
+    res.send("Something went wrong")
+  }
 })
 
 connectDB().then(() => {
-    app.listen(PORT, () => {
-        console.log(`Listening on port ${PORT}`)
-    })
-
+  app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`)
+  })
 })
