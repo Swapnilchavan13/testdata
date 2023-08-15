74e1d65529b49d2b9520e762f105e53354666117
swapnil chavan <chavanswapnil822@gmail.com>
2023-08-16 01:12:16 +0530

    test

--- index.js
+++ index.js
@@ -1,68 +1,124 @@
-require('dotenv').config();
-const express = require("express")
-const mongoose = require("mongoose")
+const express = require('express');
+const mongoose = require('mongoose');
+const multer = require('multer');
+const path = require('path');
 const cors = require("cors")
-const Book = require('./models/books')
 
+const Image = require('./models/Image');
+
+const dotenv = require('dotenv');
+
+dotenv.config();
 const app = express();
-const PORT = process.env.PORT || 3000;
+const port = 3000;
 
-mongoose.set('strictQuery', false);
+// MongoDB setup
+mongoose.connect(process.env.DB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
-const connectDB = async () => {
-  try {
-    const conn = await mongoose.connect(process.env.MONGO_URL);
-    console.log((`MongoDb Connected : ${conn.connection.host}`))
-  } catch (error) {
-    process.exit(1)
-  }
-}
-
-// Enable CORS for all routes
 app.use(cors());
 
-app.get('/', (req, res) => {
-  res.send({Title: "HomePage"})
-})
+// Image storage setup
+const storage = multer.diskStorage({
+  destination: './public/images',
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + path.extname(file.originalname));
+  },
+});
 
-app.get('/add-note', async (req, res) => {
+const upload = multer({ storage: storage });
+
+// Express middleware
+app.use(express.json());
+app.use(express.static('public'));
+
+// API route for image upload
+app.post('/api/upload', upload.single('image'), async (req, res) => {
+  const imagePath = `/images/${req.file.filename}`;
   try {
-    await Book.insertMany([
-    
-    ])
-    
+    const newImage = new Image({ imagePath });
+    await newImage.save();
+    res.json({ message: 'Image uploaded successfully', imagePath });
   } catch (error) {
-    console.log("err", + error)
+    console.error('Error uploading image:', error);
+    res.status(500).json({ message: 'Image upload failed' });
   }
-})
+});
 
-// Get single book by id
-app.get('/books/:_id', async (req, res) => {
-  try {
-    const book = await Book.findById(req.params._id);
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
+
+
+
+
+// require('dotenv').config();
+// const express = require("express")
+// const mongoose = require("mongoose")
+// const cors = require("cors")
+// const Book = require('./models/books')
+
+// const app = express();
+// const PORT = process.env.PORT || 3000;
+
+// mongoose.set('strictQuery', false);
+
+// const connectDB = async () => {
+//   try {
+//     const conn = await mongoose.connect(process.env.MONGO_URL);
+//     console.log((`MongoDb Connected : ${conn.connection.host}`))
+//   } catch (error) {
+//     process.exit(1)
+//   }
+// }
+
+// // Enable CORS for all routes
+// app.use(cors());
+
+// app.get('/', (req, res) => {
+//   res.send({Title: "HomePage"})
+// })
+
+// app.get('/add-note', async (req, res) => {
+//   try {
+//     await Book.insertMany([
     
-    if (book) {
-      res.json(book)
-    } else {
-      res.send("Book not found")
-    }
-  } catch (error) {
-    res.send(error.message)
-  }
-})
+//     ])
+    
+//   } catch (error) {
+//     console.log("err", + error)
+//   }
+// })
 
-app.get('/books', async (req, res) => {
-  const books = await Book.find();
+// // Get single book by id
+// app.get('/books/:_id', async (req, res) => {
+//   try {
+//     const book = await Book.findById(req.params._id);
+    
+//     if (book) {
+//       res.json(book)
+//     } else {
+//       res.send("Book not found")
+//     }
+//   } catch (error) {
+//     res.send(error.message)
+//   }
+// })
 
-  if (books) {
-    res.json(books)
-  } else {
-    res.send("Something went wrong")
-  }
-})
+// app.get('/books', async (req, res) => {
+//   const books = await Book.find();
 
-connectDB().then(() => {
-  app.listen(PORT, () => {
-    console.log(`Listening on port ${PORT}`)
-  })
-})
+//   if (books) {
+//     res.json(books)
+//   } else {
+//     res.send("Something went wrong")
+//   }
+// })
+
+// connectDB().then(() => {
+//   app.listen(PORT, () => {
+//     console.log(`Listening on port ${PORT}`)
+//   })
+// })
